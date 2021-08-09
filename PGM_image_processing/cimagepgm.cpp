1691e4ba9c99d40435d79d47b678729406b09242
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-08-09 09:43:22 +0200

    Add cImagePGM
    Add new class in project - cImagePGM.

---
+++ PGM_image_processing/cimagepgm.cpp
@@ -0,0 +1,20 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: main.cpp
+ */
+
+#include "cimagepgm.h"
+
+using namespace std;
+
+/********** PUBLIC: BEGIN **********/
+
+cImagePGM::cImagePGM()
+{
+}
+
+/********** PUBLIC: END **********/
+
+/* main.cpp */
+/********** END_OF_FILE **********/
---
+++ PGM_image_processing/cimagepgm.h
@@ -0,0 +1,33 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: main.cpp
+ */
+
+#ifndef CIMAGEPGM_H
+#define CIMAGEPGM_H
+
+using namespace std;
+
+/*
+ * class cImagePGM - klasa modelujaca plik graficzny
+ * w formacie PGM
+ */
+class cImagePGM
+{
+/********** PRIVATE: BEGIN **********/
+
+    \
+/********** PRIVATE: END **********/
+
+/********** PUBLIC: BEGIN **********/
+public:
+    cImagePGM();
+
+/********** PUBLIC: BEGIN **********/
+};
+
+#endif // CIMAGEPGM_H
+
+/* main.cpp */
+/********** END_OF_FILE **********/
