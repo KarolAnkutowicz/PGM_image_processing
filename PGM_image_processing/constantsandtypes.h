1d9cde0c6f5ed6a7399d09d5f0e5a830b48b2eb5
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-08-13 00:32:11 +0200

    Add types and constant
    Expansion constantsandtypes file, add new elements in this.

--- PGM_image_processing/constantsandtypes.h
+++ PGM_image_processing/constantsandtypes.h
@@ -7,6 +7,26 @@
 #ifndef CONSTANTSANDTYPES_H
 #define CONSTANTSANDTYPES_H
 
+/*
+ * enum Operations - typ wyliczeniowy zawierajacy elementy
+ * reprezentujace kolejne operacje mozliwe do wykonania
+ * na plikach graficznych
+ */
+enum Operations {negative, thresholding, multithresholding,
+                edge_detection, segmentation, seam_carving};
+
+/*
+ * enum EdgeDetection - typ wyliczeniowy zawierajacy elementy
+ * reprezentujace kolejne algorytmy wykrywania krawedzi
+ */
+enum EdgeDetection {roberts, prewitt, sobel, scharr, canny};
+
+/*
+ * const char constNameOfFile - stala przechowujaca dlugosc
+ * nazwy pliku graficznego
+ */
+const char constNameOfFile = 20;
+
 #endif // CONSTANTSANDTYPES_H
 
 /* constantsandtypes.h */
